646b3a7cd51dccfaa2b4f7338c4638cef571fe4b
HimanshuPohani <148884921+HimanshuPohani@users.noreply.github.com>
2025-01-11 23:38:24 +0530

    Update Rotate Array.py

--- Arrays/Rotate Array.py
+++ Arrays/Rotate Array.py
@@ -1,1 +1,27 @@
-
+import math
+            
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        
+        d %= n
+        cycles = maths.gcd(n,d)
+        
+        for i in range(cycles):
+            startEle = arr[i]
+            currIdx = i
+            while True:
+                nextIdx = (currIdx +d) % n
+                if nextIdx == i:
+                    break
+                
+                    
+                
+                 arr[currIdx] = arr[nextIdx]
+                 currIdx = nextIdx
+            arr[currIdx] = startEle
+            
+            
+            
+            
